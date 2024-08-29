d35d93660ae39d7617e113b49e988285a7813a89
Divyani Chhapre <divyanichhapre163@gmail.com>
2024-08-29 12:52:31 +0530

    added cors

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 require("dotenv").config();
+const cors =require("cors")
 const app = express();
 
 
 
 const PORT = process.env.PORT || 5000
 
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
